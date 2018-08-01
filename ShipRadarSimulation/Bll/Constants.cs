02576ce64644533b3d8509bfb83140610fd6bdbb
focus-on-quality <mt4.ilja@gmail.com>
2018-08-01 16:37:16 +0300

    update files

---
+++ ShipRadarSimulation/Bll/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ShipRadarSimulation.bll
+{
+    public static class Constants
+    {
+        public const double DefaultAccelerationInKnotSec = 0.05;
+        public const double DefaultAngularVelocityInGradSec = 1;
+        public const double Epselon = 0.0000001;
+    }
+}
